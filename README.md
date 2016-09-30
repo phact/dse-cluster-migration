be6e45f3c2f290f352511c3d1becdd5303f4c527
phact <estevezsebastian@gmail.com>
2016-09-30 18:48:44 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# dse-cluster-migration
+##dse-cluster-migrator
+
+This spark job will move a table from one datastax cluster to another
+
+###Build:
+
+    sbt package
+
+###RUN:
+To run in interactive mode:
+
+    dse spark-submit --class phact.MigrateTable target/scala-2.10/dse-cluster-migration_2.10-0.1.jar
+
+    dse spark-submit --class phact.MigrateTable --conf spark.dse.cluster.migration.fromClusterHost='127.0.0.1' --conf spark.dse.cluster.migration.toClusterHost='192.168.1.3' --conf spark.dse.cluster.migration.keyspace='test' --conf spark.dse.cluster.migration.table='test' target/scala-2.10/dse-cluster-migration_2.10-0.1.jar
---
+++ src/main/scala/phact/MigrateTable.scala
@@ -0,0 +1,38 @@
+package phact 
+
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql._
+import org.apache.spark.{SparkConf, SparkContext}
+
+// For DSE it is not necessary to set connection parameters for spark.master (since it will be done
+// automatically)
+object MigrateTable extends App {
+
+  val conf = new SparkConf()
+      .setAppName("dse-cluster-migration")
+
+  // A SparkContext
+  val sc = new SparkContext(conf)
+
+  var clusterHostOne        = conf.get("spark.dse.cluster.migration.fromClusterHost", null)
+  var clusterHostTwo        = conf.get("spark.dse.cluster.migration.toClusterHost", null)
+  var keyspace              = conf.get("spark.dse.cluster.migration.keyspace", null)
+  var table                 = conf.get("spark.dse.cluster.migration.table", null)
+
+  val connectorToClusterOne = CassandraConnector(sc.getConf.set("spark.cassandra.connection.host", clusterHostOne))
+  val connectorToClusterTwo = CassandraConnector(sc.getConf.set("spark.cassandra.connection.host", clusterHostTwo))
+
+  val rddFromClusterOne = {
+    // Sets connectorToClusterOne as default connection for everything in this code block
+    implicit val c = connectorToClusterOne
+    sc.cassandraTable(keyspace,table)
+  }
+
+  {
+    //Sets connectorToClusterTwo as the default connection for everything in this code block
+    implicit val c = connectorToClusterTwo
+    rddFromClusterOne.saveToCassandra(keyspace,table)
+  }
+
+
+}
